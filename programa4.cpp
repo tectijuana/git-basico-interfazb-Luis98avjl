8ecaf845d2833ddbaf06ec1b5bea2b949e7ef0be
Luis Angel Valenzuela Juarez <99373882+Luis98avjl@users.noreply.github.com>
2022-03-25 23:34:30 -0700

    Add files via upload

---
+++ programa4.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include<iostream>
+/******************************************************************************
+Problema 34
+Determinar si la suma 3^1974 + 3^1974 + 3^1974 es igual a 3^1975  
+Valenzuela Juarez Luis Angel
+@Luis98avjl
+*******************************************************************************/
+using namespace std;
+
+int main() 
+{
+    int suma;
+    double numero = 3;
+    double elevado = pow(numero, 1974);
+    double elevado1 = pow(numero, 1976);
+    
+    suma = elevado + elevado + elevado;
+    if (suma == elevado1)
+    {
+        cout << "La suma es igual" << endl;
+    }
+    return 0;
+}
