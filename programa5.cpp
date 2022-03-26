523b1b39e77988a4cf839ba82a367b1f637a3815
Luis Angel Valenzuela Juarez <99373882+Luis98avjl@users.noreply.github.com>
2022-03-25 23:35:48 -0700

    Update programa5.cpp

--- programa5.cpp
+++ programa5.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include<iostream>
 /******************************************************************************
-Problema 35 
+Problema 5 
 Escribir en un programa que determine las constantes w,x,y,z en la ecuacion WX + Y = YZ - Z e imprimir la ecuacion
 Valenzuela Juarez Luis Angel
 @Luis98avjl
