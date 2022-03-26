66d240dbcf8c21ff50747a39bbc94d0052c00c47
Luis Angel Valenzuela Juarez <99373882+Luis98avjl@users.noreply.github.com>
2022-03-25 23:33:28 -0700

    Add files via upload

---
+++ programa1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+/******************************************************************************
+ Problema 1
+ 
+Una pulgada equivale a 2.5cm, calcular el numer en cm en 32 pulgadas
+ 
+ Valenzuela Juarez Luis Angel
+ 
+ @Luis98avjl
+*******************************************************************************/
+using namespace std;
+
+int main()
+{
+    double P=2.5;
+    double x= 0;
+
+    x=32*P;
+
+    cout <<x << endl;
+    
+    return 0;
+}
